27dd1072434bb6e162717c79d003ab0571b15727
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-03-26 09:50:20 -0600

    cleaning models

--- app/db/models.py
+++ app/db/models.py
@@ -1,9 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
 Base = declarative_base()
+
+# fyi: status = 0 is inactive, status = 1 is active
+
+# Switch server_default=func.now() to default=func.now() on dates 
+# if you want SQLAlchemy to manage timestamps instead of the DB
 
 class User(Base):
     __tablename__ = "users"
@@ -38,19 +43,6 @@ class Role(Base):
     permissions = relationship("RolePermission", back_populates="role")
 
 
-class UserRole(Base):
-    __tablename__ = "users_roles"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
-    status = Column(Integer, default=1, nullable=False)
-
-    user = relationship("User", back_populates="roles")
-    role = relationship("Role", back_populates="users")
-
-
 class Permission(Base):
     __tablename__ = "permissions"
 
@@ -64,33 +56,6 @@ class Permission(Base):
     roles = relationship("RolePermission", back_populates="permission")
 
 
-class RolePermission(Base):
-    __tablename__ = "roles_permissions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    role_id = Column(Integer, ForeignKey("roles.id"))
-    permission_id = Column(Integer, ForeignKey("permissions.id"))
-    created_at = Column(DateTime, server_default=func.now()), nullable=False
-    status = Column(Integer, default=1, nullable=False)
-
-    role = relationship("Role", back_populates="permissions")
-    permission = relationship("Permission", back_populates="roles")
-
-
-class Company(Base):
-    __tablename__ = "companies"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    description = Column(String)
-    created_at = Column(DateTime, server_default=func.now(), nullable=False)
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
-    status = Column(Integer, default=1), nullable=False
-
-    evaluations_setup = relationship("EvaluationSetup", back_populates="company")
-    users = relationship("User", back_populates="company")
-
-
 class Area(Base):
     __tablename__= "areas"
 
@@ -101,9 +66,24 @@ class Area(Base):
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
     status = Column(Integer, default=1, nullable=False)
 
-    evaluations_setup = relationship("EvaluationSetup", back_populates="area")
     users = relationship("User", back_populates="area")
     skills = relationship("AreaSkill", back_populates="area")
+    companies = relationship("CompanyArea", back_populates="area")
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    status = Column(Integer, default=1, nullable=False)
+
+    users = relationship("User", back_populates="company")
+    areas = relationship("CompanyArea", back_populates="company")
+    periods = relationship("PeriodCompany", back_populates="company")
 
 
 class Period(Base):
@@ -119,8 +99,8 @@ class Period(Base):
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
     status = Column(Integer, default=1, nullable=False)
 
-    evaluations_setup = relationship("EvaluationSetup", back_populates="period")
     closed_by_user = relationship("User", back_populates="closed_periods")
+    companies = relationship("PeriodCompany", back_populates="period")
 
 
 class Skill(Base):
@@ -137,21 +117,6 @@ class Skill(Base):
     areas = relationship("AreaSkill", back_populates="skill")
     results = relationship("Result", back_populates="skill")
 
-#TODO: refactor evaluation into period_company and company_area for a better usability
-class EvaluationSetup(Base):
-    __tablename__= "evaluations_setup"
-
-    id = Column(Integer, primary_key=True, index=True)
-    period_id = Column(Integer, ForeignKey("periods.id"))
-    company_id = Column(Integer, ForeignKey("companies.id"))
-    area_id = Column(Integer, ForeignKey("areas.id"))
-    created_at = Column(DateTime, server_default=func.now(), nullable=False)
-    status = Column(Integer, default=1, nullable=False)
-
-    period = relationship("Period", back_populates="evaluations_setup")
-    company = relationship("Company", back_populates="evaluations_setup")
-    area = relationship("Area", back_populates="evaluations_setup")
-
 
 class Result(Base):
     __tablename__= "results"
@@ -159,22 +124,73 @@ class Result(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     skill_id = Column(Integer, ForeignKey("skills.id"))
-    score = Column(float)
+    score = Column(Numeric(5, 2))
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     status = Column(Integer, default=1, nullable=False)
 
     user = relationship("User", back_populates="results")
-    skill = relationship("skill", back_populates="results")
+    skill = relationship("Skill", back_populates="results")
+
+
+class UserRole(Base):
+    __tablename__ = "users_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    status = Column(Integer, default=1, nullable=False)
+
+    user = relationship("User", back_populates="roles")
+    role = relationship("Role", back_populates="users")
+    
+
+class RolePermission(Base):
+    __tablename__ = "roles_permissions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    role_id = Column(Integer, ForeignKey("roles.id"))
+    permission_id = Column(Integer, ForeignKey("permissions.id"))
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    status = Column(Integer, default=1, nullable=False)
+
+    role = relationship("Role", back_populates="permissions")
+    permission = relationship("Permission", back_populates="roles")
 
 
 class AreaSkill(Base):
     __tablename__="areas_skills"
 
     id = Column(Integer, primary_key=True, index=True)
-    area_id = Column(Integer, ForeignKey("area.id"))
+    area_id = Column(Integer, ForeignKey("areas.id"))
     skill_id = Column(Integer, ForeignKey("skills.id"))
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     status = Column(Integer, default=1, nullable=False)
 
     area = relationship("Area", back_populates="skills")
     skill = relationship("Skill", back_populates="area")
+
+class CompanyArea(Base):
+    __tablename__= "companies_areas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    area_id = Column(Integer, ForeignKey("areas.id"))
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    status = Column(Integer, default=1, nullable=False)
+
+    company = relationship("Company", back_populates="areas")
+    area = relationship("Area", back_populates="companies")
+
+
+class PeriodCompany(Base):
+    __tablename__= "periods_companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    period_id = Column(Integer, ForeignKey("periods.id"))
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    status = Column(Integer, default=1, nullable=False)
+
+    period = relationship("Period", back_populates="companies")
+    company = relationship("Company", back_populates="periods")
